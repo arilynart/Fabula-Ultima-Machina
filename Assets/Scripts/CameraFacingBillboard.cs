3411e443a8fb3ddc5a26140d4f2c3d317b46f278
Arilyn <arilyn.hess@gmail.com>
2023-03-09 16:56:44 -0700

    Added movement, animations, and post processing

--- Assets/Scripts/CameraFacingBillboard.cs
+++ Assets/Scripts/CameraFacingBillboard.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class CameraFacingBillboard : MonoBehaviour
 {
     private Camera m_Camera;
+    private SpriteRenderer renderer;
 
     private void Awake()
     {
         m_Camera = Camera.main;
+        renderer = GetComponent<SpriteRenderer>();
+        renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
     }
 
     //Orient the camera after all movement is completed this frame to avoid jittering
@@ -15,5 +18,6 @@ public class CameraFacingBillboard : MonoBehaviour
     {
         transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
             m_Camera.transform.rotation * Vector3.up);
+        renderer.sortingOrder = -Mathf.FloorToInt(transform.position.z);
     }
 }
---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MovementController : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Animator animator;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        if (input.magnitude <= 0)
+        {
+            animator.Play("Base Layer.Idle");
+            agent.destination = transform.position;
+            return;
+        }
+
+        if (Mathf.Abs(input.y) > 0 || Mathf.Abs(input.x) > 0)
+        {
+            MoveCharacter(input);
+        }
+    }
+
+    private void MoveCharacter(Vector2 input)
+    {
+        
+        if (input.x < 0)
+        {
+            animator.gameObject.transform.localScale = new Vector3(4, 4, 1);
+        }
+        else
+        {
+            animator.gameObject.transform.localScale = new Vector3(-4, 4, 1);
+        }
+
+        animator.Play("Base Layer.WalkL");
+
+        Vector3 destination = transform.position + Vector3.right * input.x + Vector3.forward * input.y;
+        agent.destination = destination;
+
+    }
+}
