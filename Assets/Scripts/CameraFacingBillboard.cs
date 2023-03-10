4b3d4efae8c26268bab9bc5b9144fc7c4ccd6a70
Arilyn <arilyn.hess@gmail.com>
2023-03-09 17:09:25 -0700

    Minor Tweaks before Merge

--- Assets/Scripts/CameraFacingBillboard.cs
+++ Assets/Scripts/CameraFacingBillboard.cs
@@ -4,13 +4,13 @@ using System.Collections;
 public class CameraFacingBillboard : MonoBehaviour
 {
     private Camera m_Camera;
-    private SpriteRenderer renderer;
+    private SpriteRenderer spriteRenderer;
 
     private void Awake()
     {
         m_Camera = Camera.main;
-        renderer = GetComponent<SpriteRenderer>();
-        renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
     }
 
     //Orient the camera after all movement is completed this frame to avoid jittering
@@ -18,6 +18,6 @@ public class CameraFacingBillboard : MonoBehaviour
     {
         transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
             m_Camera.transform.rotation * Vector3.up);
-        renderer.sortingOrder = -Mathf.FloorToInt(transform.position.z);
+        spriteRenderer.sortingOrder = -Mathf.FloorToInt(transform.position.z);
     }
 }
